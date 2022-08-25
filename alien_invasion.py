58ffa8e908347856bf56af649166806f87f5b64f
Ranorien <ranorien@gmail.com>
2022-08-25 17:10:42 +0300

    New version

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,6 @@
-from re import A
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion():
     """main class"""
@@ -8,19 +8,28 @@ class AlienInvasion():
     def __init__(self):
         pygame.init()
 
+        # Get settings
+        self.settings = Settings()
+
+
         # set paramters of screen
-        self.screen = pygame.display.set_mode((1280, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('My the first game - Alien Invasion')
+
 
     def run_game(self):
         """main loop on the screen"""
         while True:
             # Check events of keyboard and mouse
             for event in pygame.event.get():
+                # print(event) shows events IDs in terminal
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # Show last screen
+            # Set default color
+            self.screen.fill(self.settings.bg_color)
+
+            # apply screen changes
             pygame.display.flip()
 
 
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """saved settings for project"""
+
+    def __init__(self):
+        """init settings"""
+
+        # screen settings group:
+        self.screen_width = 1280
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230) # default screen background color
